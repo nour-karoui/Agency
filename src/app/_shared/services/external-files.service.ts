99e8a33a7b5c88c4ec76fb7ccad5032c3d2244c7
Chedyh <chedyzapa@gmail.com>
2020-05-07 11:04:09 +0100

    shadi push

--- src/app/_shared/services/external-files.service.ts
+++ src/app/_shared/services/external-files.service.ts
@@ -8,13 +8,13 @@ export class ExternalFilesService {
 
   constructor() {
     this.loadScript('../assets/js/jquery-migrate-3.0.1.min.js');
-    // this.loadScript('../assets/js/jquery.easing.1.3.js');
+    this.loadScript('../assets/js/jquery.easing.1.3.js');
     this.loadScript('../assets/js/jquery.waypoints.min.js');
     this.loadScript('../assets/js/jquery.stellar.min.js');
     this.loadScript('../assets/js/jquery.animateNumber.min.js');
     this.loadScript('../assets/js/owl.carousel.min.js');
     this.loadScript('../assets/js/jquery.magnific-popup.min.js');
-    // this.loadScript('../assets/js/scrollax.min.js');
+    this.loadScript('../assets/js/scrollax.min.js');
     // this.loadScript('../assets/js/google-map.js');
     this.loadScript('../assets/js/main.js');
     this.loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false');
