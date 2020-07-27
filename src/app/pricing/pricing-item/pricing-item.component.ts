bdd263254ba8a74dac36109ca08e6ff116429651
nour karoui <nkaroui1998@gmail.com>
2020-07-27 15:40:03 +0100

    fix pricing carousel

--- src/app/pricing/pricing-item/pricing-item.component.ts
+++ src/app/pricing/pricing-item/pricing-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pricing-item',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pricing-item.component.css']
 })
 export class PricingItemComponent implements OnInit {
-
+  @Input() pricing;
   constructor() { }
 
   ngOnInit() {
--- src/app/pricing/pricing.component.ts
+++ src/app/pricing/pricing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxCarousel } from 'ngx-carousel';
+
 
 @Component({
   selector: 'app-pricing',
@@ -6,10 +8,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pricing.component.css']
 })
 export class PricingComponent implements OnInit {
-
+  public carouselOne: NgxCarousel;
+  paused = false;
+  unpauseOnArrow = false;
+  pauseOnIndicator = false;
+  pauseOnHover = true;
+  pricings = [{
+      name: 'Basic',
+      price: 35,
+      included: ['Video Editing', 'Scriptwriting', 'Free Thumbnail', '3 Days Delivery',
+        '1 Length Variation', '1 Size Orientation', 'Royalty Free Music', '40 Seconds Running Time'],
+      notIncluded: 'Scroll Stopper'
+    },
+    {
+      name: 'Standard',
+      price: 40,
+      included: ['Video Editing', 'Scriptwriting', 'Free Thumbnail', '3 Days Delivery',
+        '1 Length Variation', '1 Size Orientation', 'Royalty Free Music', '40 Seconds Running Time', 'Scroll Stopper'],
+    },
+    {
+      name: 'Premium',
+      price: 55,
+      included: ['Video Editing', 'Scriptwriting', '2 Free Thumbnails', '3 Days Delivery',
+        '1 Length Variation', '1 Size Orientation', 'Royalty Free Music', '40 Seconds Running Time', '2 Scroll Stoppers'],
+    }];
   constructor() { }
 
   ngOnInit() {
+    this.carouselOne = {
+      grid: {xs: 1, sm: 1, md: 2, lg: 3, all: 0},
+      slide: 1,
+      speed: 400,
+      interval: 4000,
+      point: {
+        visible: true
+      },
+      load: 2,
+      touch: true,
+      loop: true,
+      custom: 'banner'
+    };
   }
 
 }
